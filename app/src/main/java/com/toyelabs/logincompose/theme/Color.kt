7655b322bfbe8a6464301acb03a0338e4746d276
Mannyking <68853328+Mannyking@users.noreply.github.com>
2024-11-16 14:54:26 +0100

    Theme, Colors, and Fonts Configuration
    - introduced doto font
    
    - disabled material you
    
    - added a variety of colors

--- app/src/main/java/com/toyelabs/logincompose/theme/Color.kt
+++ app/src/main/java/com/toyelabs/logincompose/theme/Color.kt
@@ -9,3 +9,63 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val NavyBlue100 = Color(0xFF4C5CD0)
+val NavyBlue300 = Color(0xFF004C90)
+val NavyBlue500 = Color(0xFF002D5F)
+val NavyBlue700 = Color(0xFF142540)
+
+val PowderBlue500 = Color(0xFF476EA4)
+
+val TurquoiseBlue100 = Color(0xFFB3E5FC)
+val TurquoiseBlue300 = Color(0xFF4FC3F7)
+val TurquoiseBlue500 = Color(0xFF03A9F4)
+val TurquoiseBlue700 = Color(0xFF0288D1)
+
+val Transparent = Color(0x00FFFFFF)
+
+val LightGreen100 = Color(0xFFB2EAC1)
+val LightGreen400 = Color(0xFF66D483)
+val Green300 = Color(0xFF4CAF50)
+val Green500 = Color(0xFF00FF00)
+val Aqua300 = Color(0xFF80DB98)
+val Aqua500 = Color(0xFF00FFBF)
+val Lemon300 = Color(0xFFADD136)
+val Lemon500 = Color(0xFF8BC34A)
+val DarkGreen500 = Color(0xFF2B4928)
+val DarkGreen700 = Color(0xFF00401A)
+val Teal500 = Color(0xFF018786)
+
+val DarkGray500 = Color(0xFF323232)
+val DarkGray300 = Color(0xFF555555)
+val DarkGray700 = Color(0xFF1A1A1A)
+val Gray100 = Color(0xFFE3E3E3)
+val Gray500 = Color(0xFF959595)
+val Gray600 = Color(0xFF848485)
+
+val Purple500 = Color(0xFF800080)
+val Orange500 = Color(0xFFFF5800)
+val Peach700 = Color(0xFFF6313F)
+val Red500 = Color(0xFFFF0000)
+val Pink500 = Color(0xFFFFC0CB)
+val Orange300 = Color(0xFFFF8F00)
+val Yellow500 = Color(0xFFFFFF00)
+val HoloRedDark = Color(0xFFCC0000)
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
+
+/* ---------------------------colors that pertain to views and similar--------------------------- */
+val TextDark = Black
+val TextLight = White
+
+val BackgroundLight = White
+val BackgroundDark = Black
+
+val ColorPrimary = NavyBlue300
+val ColorPrimaryDark = TurquoiseBlue300
+
+val ColorSecondary = NavyBlue500
+val ColorSecondaryDark = TurquoiseBlue500
+
+val ColorTertiary = LightGreen400
+val ColorTertiaryDark = LightGreen100
--- app/src/main/java/com/toyelabs/logincompose/theme/Type.kt
+++ app/src/main/java/com/toyelabs/logincompose/theme/Type.kt
@@ -2,9 +2,11 @@ package com.toyelabs.logincompose.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.toyelabs.logincompose.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -31,4 +33,28 @@ val Typography = Typography(
         letterSpacing = 0.5.sp
     )
     */
+)
+
+val DotoFontFamily = FontFamily(
+    Font(R.font.doto_black, FontWeight.Black),
+    Font(R.font.doto_bold, FontWeight.Bold),
+    Font(R.font.doto_extrabold, FontWeight.ExtraBold),
+    Font(R.font.doto_extralight, FontWeight.ExtraLight),
+    Font(R.font.doto_light, FontWeight.Light),
+    Font(R.font.doto_medium, FontWeight.Medium),
+    Font(R.font.doto_regular, FontWeight.Normal),
+    Font(R.font.doto_semibold, FontWeight.SemiBold),
+    Font(R.font.doto_thin, FontWeight.Thin)
+)
+
+val DotoRoundedFontFamily = FontFamily(
+    Font(R.font.doto_rounded_black, FontWeight.Black),
+    Font(R.font.doto_rounded_bold, FontWeight.Bold),
+    Font(R.font.doto_rounded_extrabold, FontWeight.ExtraBold),
+    Font(R.font.doto_rounded_extralight, FontWeight.ExtraLight),
+    Font(R.font.doto_rounded_light, FontWeight.Light),
+    Font(R.font.doto_rounded_medium, FontWeight.Medium),
+    Font(R.font.doto_rounded_regular, FontWeight.Normal),
+    Font(R.font.doto_rounded_semibold, FontWeight.SemiBold),
+    Font(R.font.doto_rounded_thin, FontWeight.Thin)
 )
